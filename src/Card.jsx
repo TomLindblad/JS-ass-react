baf5236780108176469b7c62cdf4af96e164a74a
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2024-11-20 09:52:18 +0100

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+import './Card.css'
+
+function Bookcard(props) {
+
+    let shoplink = ""
+    let title = "booktitle";
+    let bookcover = "bookcover";
+    let price = "bookprice";
+    
+
+    return (
+    <div className="bookcard-container" ><a href={props.shoplink}>
+      <img className="book-cover" src={props.bookPic} alt="bookcover"/>
+      <div className="bookcard-textcontainer">
+        <h3 className="book-title">{props.title}</h3>
+        <p className="book-pricetag">{props.price}:-</p>
+      </div>
+    </a></div>
+  )
+}
+
+export default Bookcard;
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+function Header() {
+
+    return (
+        <div className="header-container">
+            <h1 className="mainTitle">Tom's bookcircle</h1>
+            <h3 className="subTitle">“The more that you read, the more things you will know. The more that you learn, the more places you'll go.” - Dr. Seuss</h3>
+        </div>
+    )
+  }
+  
+  export default Header;
