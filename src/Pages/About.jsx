25b9a220a2f3eef623c6229808551e430a36bbd5
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2024-11-22 21:39:43 +0100

    added buttons

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,24 @@
 function About() {
 
+  let number = 0;
+  console.log (number)
+  const addOne = () => {
+    number++;
+    console.log (number);
+  };
+
+  const removeOne = () => {
+    number--;
+    console.log (number);
+  };
+
+
     return (
       <>
         <h1>ABOUT</h1>
+        <button className="plus-btn" onClick={addOne}>ADD</button>
+        <h2 className="myNumber">0</h2>
+        <button className="minus-btn" onClick={removeOne}>SUBSTRACT</button>
       </>
     );
   }
