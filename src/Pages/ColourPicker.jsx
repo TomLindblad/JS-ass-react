d4a01d1061741f5928193cfdf3244701e5228735
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2024-11-27 10:15:48 +0100

    made the colour picker work

--- src/Pages/ColourPicker.jsx
+++ src/Pages/ColourPicker.jsx
@@ -1,14 +1,30 @@
+import '../colourPicker.css';
+import { useState } from 'react';
+
+
 function ColourPicker() {
 
+  const [colour, setColour] = useState("#B6AD26");
+  const myStyle = {
+    backgroundColor: colour
+  }
+
+    function handleColourChange(event){
+      setColour(event.target.value);
+    };
   
   
       return (
         <>
-          <h2>Pick a colour and it will change the world.</h2>
-          <input type="color"></input>
-
+          <div className="colourPickerContainer">
+            <h2>Pick a colour and change the world.</h2>
+            <div className="colourBox" style={myStyle}>
+              <p className="colourBoxText">{colour}</p>
+            </div>
+            <input type="color" value={colour} onChange={handleColourChange} />            
+          </div>
         </>
       );
-    }
+    };
     
     export default ColourPicker;
